3e6ec680f20e8839dc7400f5d2cdcf755320cbf1
kaahila <dr.doomxp@gmail.com>
2023-11-12 12:12:32 +0100

    cool

--- arm/servo.py
+++ arm/servo.py
@@ -17,11 +17,6 @@ class Servo:
         self.angle = 0
         self.pin = pin
 
-    def __del__(self) -> None:
-        """Close servo connection."""
-
-        self.close()
-
     def _mapAngle(self, angle: float) -> float:
         """Maps angle to PWM value."""
         return numpy.interp(angle, [0, 180], [500, 2500])
@@ -40,5 +35,8 @@ class Servo:
 
     def close(self) -> None:
         """Manually close servos."""
-        self.pwm.set_PWM_dutycycle(self.pin, 0)
-        self.pwm.set_PWM_frequency(self.pin, 0)
+        try:
+            self.pwm.set_PWM_dutycycle(self.pin, 0)
+            self.pwm.set_PWM_frequency(self.pin, 0)
+        except:
+            pass
