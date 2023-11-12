95cfc15705c96af42083ae455fc83f1eb7a9448c
kaahila <dr.doomxp@gmail.com>
2023-11-12 12:01:28 +0100

    cool

--- arm/servo.py
+++ arm/servo.py
@@ -1,6 +1,7 @@
 """Servo interface"""
 
 import RPi.GPIO as GPIO
+import pigpio
 import numpy
 
 
@@ -11,10 +12,10 @@ class Servo:
         """Setup servo. Assumes already initialized GPIO."""
 
         GPIO.setup(pin, GPIO.OUT)
-        
-        self._servo = GPIO.PWM(pin, 50)
-        self._servo.start(0)
+        self.pwm = pigpio.pi()
+        self.pwm.set_PWM_frequency(pin, 50)
         self.angle = 0
+        self.pin = pin
 
     def __del__(self) -> None:
         """Close servo connection."""
@@ -23,22 +24,21 @@ class Servo:
 
     def _mapAngle(self, angle: float) -> float:
         """Maps angle to PWM value."""
+        return numpy.interp(angle, [0, 180], [500, 2500])
 
-        return numpy.interp(angle, [0, 180], [2, 12])
-    
     def setAngle(self, angle: float = None) -> None:
         """Set the current angle of the servo."""
 
-        angle = numpy.min((numpy.max((angle, 0)), 180))     # clip to [0, 180]
-        self._servo.ChangeDutyCycle(self._mapAngle(angle))
+        angle = numpy.min((numpy.max((angle, 0)), 180))
+        self.pwm.set_servo_pulsewidth(self.pin, self._mapAngle(angle))
         self.angle = angle
 
     def getAngle(self) -> float:
         """Return internal angle state."""
 
         return self.angle
-    
+
     def close(self) -> None:
         """Manually close servos."""
-
-        self._servo.stop()
+        self.pwm.set_PWM_dutycycle(self.pin, 0)
+        self.pwm.set_PWM_frequency(self.pin, 0)
